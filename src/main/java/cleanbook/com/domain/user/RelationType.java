8333cd3d023589df6587aef760938e66d31e9964
lacram <chemin9898@gmail.com>
2022-07-06 09:57:32 +0900

    test

---
+++ src/main/java/cleanbook/com/domain/user/RelationType.java
@@ -0,0 +1,5 @@
+package cleanbook.com.domain.user;
+
+public enum RelationType {
+    FOLLOW, BAN, REPORT, LIKE
+}
--- src/main/java/cleanbook/com/repository/FollowRepository.java
+++
@@ -1,7 +0,0 @@
-package cleanbook.com.repository;
-
-import cleanbook.com.domain.user.Follow;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FollowRepository extends JpaRepository<Follow, Long> {
-}
