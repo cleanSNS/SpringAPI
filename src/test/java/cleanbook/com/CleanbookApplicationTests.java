667f5276c5df68c5f872135aa6b34c9eb78a0125
lacram <chemin9898@gmail.com>
2022-09-16 19:09:52 +0900

    jenkins test

--- src/test/java/cleanbook/com/CleanbookApplicationTests.java
+++ src/test/java/cleanbook/com/CleanbookApplicationTests.java
@@ -2,7 +2,6 @@ package cleanbook.com;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class CleanbookApplicationTests {
