7f3595db50f47591486f7a0c6114daf35dfaf31e
lacram <chemin9898@gmail.com>
2022-10-11 19:16:01 +0900

    chat update

--- src/main/java/cleanbook/com/dto/chat/ChatDto.java
+++ src/main/java/cleanbook/com/dto/chat/ChatDto.java
@@ -7,10 +7,18 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
 public class ChatDto {
     private String sender;
     private String message;
     private LocalDateTime createdDate;
 
+    public ChatDto() {
+    }
+
+    public ChatDto(String sender, String message, LocalDateTime createdDate) {
+        this.sender = sender;
+        this.message = message;
+        this.createdDate = createdDate;
+    }
 }
